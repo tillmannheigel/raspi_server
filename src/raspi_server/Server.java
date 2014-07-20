ac826c37b649f1be91c50e6d4255448614905745
tillmannheigel <heigel@tillmannheigel.de>
2014-07-20 16:58:16 +0200

    implemented Server as Runnable

--- src/raspi_server/Server.java
+++ src/raspi_server/Server.java
@@ -4,24 +4,30 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Server {
+public class Server implements Runnable{
 	
 	private ServerSocket mySocket;
 	private Socket myClient;
+	private int myPort;
 
-	Server(int port){
-		System.out.println("Startet new Server on port " + port + ".");
+	public Server(int port) {
+		myPort = port;
+	}
 
-		try {
-			mySocket = new ServerSocket(port);
-			myClient = mySocket.accept();
-			System.out.println(myClient.toString());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			System.out.println("Stopped Server");
-			e.printStackTrace();
+	@Override
+	public void run() {
+		System.out.println("Startet new Server on port " + myPort + ".");
+		while (true) {
+			try {
+				mySocket = new ServerSocket(myPort);
+				myClient = mySocket.accept();
+				System.out.println(myClient.toString());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				System.out.println("Stopped Server");
+				e.printStackTrace();
+			}
 		}
 	}
-	
 	
 }
--- src/raspi_server/ServerMain.java
+++ src/raspi_server/ServerMain.java
@@ -4,6 +4,8 @@ public class ServerMain {
 	
 	public static void main(String[] args) {
 		Server server = new Server(50296);
+		Thread serverThread = new Thread(server);
+		serverThread.start();
 	}
 
 }
